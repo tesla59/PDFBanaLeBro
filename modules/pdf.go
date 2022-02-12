7fb2f8c2295b2fb5919903bfde8a5086f0098d47
tesla59 <talktonishantsingh.ns@gmail.com>
2022-02-12 23:35:09 +0530

    PDF: improve image fetching mechanism yet again Use correct if else logic format code

--- modules/pdf.go
+++ modules/pdf.go
@@ -54,49 +54,49 @@ func PDF(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if (DB.Where(&Session{UserID: m.Author.ID}).First(&session)).Error != nil {
 			// User not in DB
 			s.ChannelMessageSend(m.ChannelID, "I dont even know who u are\nSend soja.start to send me your bank details")
-		} else {
-			// User in DB
-			if session.RState {
-				// User in DB + RState True
-				if len(m.Attachments) == 0 {
-					// soja.f without attachments
-					s.ChannelMessageSend(m.ChannelID, "Error: no file sent")
-				} else {
-					// soja.f with attachment
-					for i := range m.Attachments {
-						filePath := session.UserID + "/" + "temp.jpeg"
-						if Err = dload.DownloadFile(m.Attachments[i].ProxyURL, filePath); Err != nil {
-							log.Println("Error downloading file: ", Err)
-							return
-						}
-						if isImage(filePath) {
-							// File is Image
-							if i == 0 {
-								// Only send this once
-								s.ChannelMessageSend(m.ChannelID, "Hippity Hoppty your images are now my property")
-							}
-							// Creating a new PDF/Appending to existing one
-							imp, _ := api.Import("form:A3, pos:c, s:1.0", pdfcpu.POINTS)
-							filePDF := session.UserID + "/" + m.Author.Username + ".pdf"
-							Err = api.ImportImagesFile([]string{filePath}, filePDF, imp, nil)
-							if Err != nil {
-								log.Println("Error Creating output PDF: ", Err)
-								return
-							}
-							os.Remove(filePath)
-							session.CurrentImages++
-							DB.Save(&session)
-						} else {
-							// Unsupported filetype
-							s.ChannelMessageSend(m.ChannelID, "Error: This file format is not supported")
-							os.Remove(filePath)
-						}
-					}
-				}
-			} else {
-				// User in DB + RState False
-				s.ChannelMessageSend(m.ChannelID, "You dont have an active session\nSend soja.start to enable a session")
+			return
+		}
+		// User not in DB
+		if !session.RState {
+			// User in DB + RState False
+			s.ChannelMessageSend(m.ChannelID, "You dont have an active session\nSend soja.start to enable a session")
+			return
+		}
+		// User in DB + RState True
+		if len(m.Attachments) == 0 {
+			// soja.f without attachments
+			s.ChannelMessageSend(m.ChannelID, "Error: no file sent")
+			return
+		}
+		// soja.f with attachment
+		for i := range m.Attachments {
+			filePath := session.UserID + "/" + "temp.jpeg"
+			if Err = dload.DownloadFile(m.Attachments[i].ProxyURL, filePath); Err != nil {
+				log.Println("Error downloading file: ", Err)
+				return
 			}
+			if !isImage(filePath) {
+				// Unsupported filetype
+				s.ChannelMessageSend(m.ChannelID, "Error: This file format is not supported")
+				os.Remove(filePath)
+				return
+			}
+			// File is Image
+			if i == 0 {
+				// Only send this once
+				s.ChannelMessageSend(m.ChannelID, "Hippity Hoppty your images are now my property")
+			}
+			// Creating a new PDF/Appending to existing one
+			imp, _ := api.Import("form:A3, pos:c, s:1.0", pdfcpu.POINTS)
+			filePDF := session.UserID + "/" + m.Author.Username + ".pdf"
+			Err = api.ImportImagesFile([]string{filePath}, filePDF, imp, nil)
+			if Err != nil {
+				log.Println("Error Creating output PDF: ", Err)
+				return
+			}
+			os.Remove(filePath)
+			session.CurrentImages++
+			DB.Save(&session)
 		}
 	}
 
